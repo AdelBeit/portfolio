610e4614767340cdc3a81599bb4c748c9efddfff
BreadCrumb <adelbeit@gmail.com>
2022-08-20 23:26:32 -0700

    deploy service resume

---
+++ pages/service.tsx
@@ -0,0 +1,3 @@
+import { App } from "../src/components/service/ServiceResume";
+
+export default App;
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,3 @@
-import { App as Service } from "./components/service/ServiceResume";
 import { App as Development } from "./components/development/DevelopmentResume";
 
-// export default Service;
 export default Development;
-
-// export default function App() {
-//   const resume = "service";
-//   return <div> {Service} </div>;
-// }
