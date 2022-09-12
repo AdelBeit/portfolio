ad39756901064db7e66f994bc1600167ebf84124
adel <adelbeit@gmail.com>
2022-09-11 21:45:58 -0700

    update service resume data

--- src/service_data.ts
+++ src/service_data.ts
@@ -12,6 +12,7 @@ export const SiteData = {
         "Gather requirements, design, build, and deliver a fast and sleek landing page for an auto mechanic shop.",
         "Delivered the project on time.",
       ],
+      url: { demo: "", github: "" }
     },
     {
       company: "Indiana University Information Technology Services",
@@ -22,6 +23,7 @@ export const SiteData = {
         "Entrusted with keys for multiple labs with $10,000+ in equipment.",
         "Troubleshoot broken lab equipment and file reports when necessary.",
       ],
+      url: { demo: "", github: "" }
     },
     {
       company: "Kroger",
@@ -31,6 +33,7 @@ export const SiteData = {
         "Scanned and interacted with 100+ customers a day in a fast, friendly, and efficient manner.",
         "Handled upwards of $1000 of cash per day, and was in charge of emptying the self checkout tills at the end of the day.",
       ],
+      url: { demo: "", github: "" }
     },
 
     {
@@ -41,6 +44,7 @@ export const SiteData = {
         "Output 100+ orders per day, working on multiple different orders simultaneously, in a fast and precise manner maximizing custumer satisfaction.",
         "Operated high-volume cooking equipment such as grills and deep fryers in a fast paced environment.",
       ],
+      url: { demo: "", github: "" }
     },
   ],
   education: {
