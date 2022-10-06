2afc06be99ccbef793bedfc1abf98989a0f694d6
Adele <adelbeit@gmail.com>
2022-10-06 16:44:52 -0700

    context hook => context factory<T>, + serviceContext, + devContext

--- src/hooks/use_site_context.tsx
+++ src/hooks/use_site_context.tsx
@@ -1,23 +1,29 @@
-import { createContext, useContext } from "react";
+import { Context, createContext, useContext } from "react";
 import { SiteData as DevelopmentData } from "../development_data";
 import { SiteData as ServiceData } from "../service_data";
 
-const Context = { ...DevelopmentData, ...ServiceData };
+function createUseSite<T>(SiteContext: Context<T>) {
+  return () => {
+    const site = useContext<T>(SiteContext);
 
-const SiteContext = createContext<Partial<typeof Context> | undefined>(
-  undefined
-);
+    if (!site) {
+      console.trace("useSite must be used within a PortfolioProvider");
+      throw new Error("useSite must be used within a PortfolioProvider");
+    }
 
-function useSite() {
-  const site = useContext(SiteContext);
-
-  if (!site) {
-    console.trace("useSite must be used within a PortfolioProvider");
-    throw new Error("useSite must be used within a PortfolioProvider");
-  }
-
-  return site;
+    return site;
+  };
 }
 
-export type { Context };
-export { useSite, SiteContext };
+function createSiteContext<T>() {
+  const SiteContext = createContext<T | null>(null);
+  const useSite = createUseSite(SiteContext);
+  return { SiteContext, useSite };
+}
+
+const { SiteContext: ServiceContext, useSite: useService } =
+  createSiteContext<typeof ServiceData>();
+const { SiteContext: DevContext, useSite: useDev } =
+  createSiteContext<typeof DevelopmentData>();
+
+export { ServiceContext, useService, DevContext, useDev };
