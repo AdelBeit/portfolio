763ccedb2e8d014dc981fe9dc926810490bc2ce0
Adele <adelbeit@gmail.com>
2022-10-06 17:04:23 -0700

    service: change header to show number and city

--- src/components/service/Header.tsx
+++ src/components/service/Header.tsx
@@ -3,15 +3,12 @@ import { useService } from "../../hooks/use_site_context";
 import styles from "./Header.module.css";
 
 function Header() {
-  const { name, role, site, description } = useService();
+  const { name, role, site, description, phone, city } = useService();
   return (
     <div className={styles.container__header}>
       <span className={styles.name}>{name}</span>
       <div className={styles.section__info}>
-        <span className={styles.info}>
-          {`${role}   |   `}
-          <a href={`https://${site}`}>{site}</a>
-        </span>
+        <span className={styles.info}>{`${phone}     |     ${city}`}</span>
       </div>
       <div className={styles.section__description}>
         <span className={styles.description}>{description}</span>
