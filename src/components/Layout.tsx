41c9e9de8accc7b8f9b4b78b2e238164213a9736
Adele <adelbeit@gmail.com>
2022-10-19 01:24:15 -0700

    +favicon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,18 @@
 import Head from "next/head";
 import React from "react";
 
-export default function Layout({
-  children,
-  title = "Adele",
-}: {
+interface LayoutProps {
   children: React.ReactNode;
   title?: string;
-}) {
+}
+
+export default function Layout({ children, title = "Adele" }: LayoutProps) {
+  const favicon = "/icons/code.png";
   return (
     <>
       <Head>
         <title>{title}</title>
-        <link rel="icon" href="./favicon.png" type="image/x-icon"></link>
+        <link rel="icon" href={favicon} type="image/x-icon"></link>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
