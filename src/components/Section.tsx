9bcdd268cdc4c2612fa218d8f59eb1db3bcd8b9c
Adele <adelbeit@gmail.com>
2022-10-19 01:27:02 -0700

    update section component

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -2,18 +2,16 @@ import classNames from "classnames";
 import React from "react";
 import styles from "./Section.module.css";
 
-function Section({
-  title = "",
-  children,
-  classes = "",
-}: {
+interface SectionProps {
   title?: string;
   children: React.ReactNode;
   classes?: string | string[];
-}) {
+}
+
+function Section({ title = "", children, classes = "" }: SectionProps) {
   title;
   return (
-    <div className={classNames(styles["container__section"], classes)}>
+    <div className={classNames(styles.container, classes)}>
       {title ? <h2>{title}</h2> : null}
       {children}
     </div>
