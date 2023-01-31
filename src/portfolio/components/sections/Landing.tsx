bf2f9941b05a00700631868638d14e0907f94b44
Adele <adelbeit@gmail.com>
2023-01-31 08:15:42 -0800

    animated content box on landing page

--- src/portfolio/components/sections/Landing.tsx
+++ src/portfolio/components/sections/Landing.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Typer from "../../utils/Typer";
 
 interface Props {
   title: string;
@@ -8,6 +9,10 @@ interface Props {
 }
 
 export default function Landing({ title, role, content, keywords }: Props) {
+  useEffect(() => {
+    const typer = new Typer("._contentBox.content span", content);
+    return () => typer.destroy();
+  }, []);
   return (
     <div id="_landing" className="_section">
       <div className="_container relative">
@@ -18,7 +23,7 @@ export default function Landing({ title, role, content, keywords }: Props) {
           <span>{role}</span>
         </div>
         <div className="_contentBox content extra-small outline-neon">
-          <span>{content}</span>
+          <span></span>
         </div>
       </div>
       <style jsx>{`
@@ -39,10 +44,17 @@ export default function Landing({ title, role, content, keywords }: Props) {
           padding: 15px;
           margin-left: -15px;
           background: rgba(40, 40, 40, 0.5);
+          max-width: 800px;
         }
+
         @media only screen and (min-height: 800px) {
           ._container {
             gap: 15vh;
+          }
+        }
+        @media only screen and (min-width: 1401px) {
+          ._container {
+            padding-left: 10%;
           }
         }
         @media only screen and (max-width: 1400px) {
