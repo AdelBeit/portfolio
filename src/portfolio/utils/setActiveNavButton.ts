646413f4a5e4d926ad8a5d89b71c3aefd66ecf0b
Adele <adelbeit@gmail.com>
2023-02-02 11:33:33 -0800

    navbutton active status is determined by intersection observer not by mouse click target

--- src/portfolio/utils/setActiveNavButton.ts
+++ src/portfolio/utils/setActiveNavButton.ts
@@ -1,32 +1,6 @@
 import { SECTIONS } from "../types";
 
-export function markActive(e: MouseEvent) {
-  let current = e.target as HTMLElement;
-  const illegalIcons = [
-    "music",
-    "arrowup",
-    "arrowdown",
-    "github",
-    "linkedin",
-    "email",
-    "resume",
-  ];
-  if (current.id.includes("_navbar")) return false;
-
-  while (!current.classList.contains("_button")) {
-    current = current.parentElement;
-  }
-
-  if (illegalIcons.some((string) => current.classList.contains(string))) return;
-
-  let parent = current.parentElement;
-
-  parent.querySelector("._button.active")?.classList.remove("active");
-  current.classList.add("active");
-}
-
 export function setActiveNavButton(section: SECTIONS) {
-  console.log(section);
   document.querySelector(`._button.active`)?.classList.remove("active");
   document.querySelector(`._button.${section}`)?.classList.add("active");
 }
