71d68a3c7892b416dfca1173ae127eeb0ae9dbc3
Adele <adelbeit@gmail.com>
2023-02-02 01:40:40 -0800

    rename markactive.ts, add new function for activating diff nav buttons

--- src/portfolio/utils/setActiveNavButton.ts
+++ src/portfolio/utils/setActiveNavButton.ts
@@ -1,6 +1,16 @@
+import { SECTIONS } from "../types";
+
 export function markActive(e: MouseEvent) {
   let current = e.target as HTMLElement;
-  const illegalIcons = ["music", "arrowup", "arrowdown", "github", "linkedin", "email", "resume"];
+  const illegalIcons = [
+    "music",
+    "arrowup",
+    "arrowdown",
+    "github",
+    "linkedin",
+    "email",
+    "resume",
+  ];
   if (current.id.includes("_navbar")) return false;
 
   while (!current.classList.contains("_button")) {
@@ -13,4 +23,10 @@ export function markActive(e: MouseEvent) {
 
   parent.querySelector("._button.active")?.classList.remove("active");
   current.classList.add("active");
+}
+
+export function setActiveNavButton(section: SECTIONS) {
+  console.log(section);
+  document.querySelector(`._button.active`)?.classList.remove("active");
+  document.querySelector(`._button.${section}`)?.classList.add("active");
 }
