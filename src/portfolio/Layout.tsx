39901ef5d80c4cdfde9a10f621ffb0130fdc5575
Adele <adelbeit@gmail.com>
2023-02-07 09:43:37 -0800

    add a layout file for <head>

---
+++ src/portfolio/Layout.tsx
@@ -0,0 +1,39 @@
+import Head from "next/head";
+import React from "react";
+
+interface LayoutProps {
+  children: React.ReactNode;
+  title?: string;
+}
+
+export default function Layout({
+  children,
+  title = "Adele Beitvashahi",
+}: LayoutProps) {
+  const favicon = "/icons/code.png";
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+
+        {/* <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="apple-touch-icon" href="/app_icons/icon-192x192.png" />
+        <meta name="theme-color" content="#fff" /> */}
+
+        <meta charSet="utf-8" />
+        {/* <link rel="icon" href={favicon} type="image/x-icon"></link> */}
+
+        {/* <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="application-name" content="Adele" />
+        <meta name="apple-mobile-web-app-title" content="Adele" />
+        <meta name="msapplication-starturl" content="/" /> */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=yes"
+        ></meta>
+      </Head>
+      {children}
+    </>
+  );
+}
--- src/portfolio/Portfolio.tsx
+++ src/portfolio/Portfolio.tsx
@@ -14,6 +14,7 @@ import Landing from "./components/sections/Landing";
 import { LANDING } from "../../public/portfolio.data";
 import { SECTIONS } from "./types";
 import { useWidth } from "./store/WidthStore";
+import Layout from "./Layout";
 
 /*
 css cyberpunk buttons https://codepen.io/jh3y/full/BajVmOg
@@ -74,97 +75,99 @@ export function App() {
   }, []);
 
   return (
-    <div className="_container relative">
-      <IconEther />
-      <ContentBox handleIntersection={handleIntersection}>
-        <Landing
-          title={LANDING.NAME}
-          role={LANDING.ROLE}
-          description={LANDING.CONTENT}
-          keywords={LANDING.KEYWORDS}
-          isInView={currentSection === "_landing"}
-        />
-        <About isInView={currentSection === "_about"} />
-        <Product isInView={currentSection === "_product"} />
-        {/* <BlogPost isInView={currentSection === "_blogpost"} /> */}
-        <Experience isInView={currentSection === "_experience"} />
-      </ContentBox>
-      <NavBox showLanding={currentSection === "_landing"} />
-      <style jsx>{`
-        ._container {
-          height: 100%;
-          width: 100%;
-          color: var(--green);
-          background-color: var(--black);
-
-          padding: 0;
-          margin: 0;
-
-          font-family: Roboto;
-          font-weight: bold;
-
-          overflow: hidden;
-          display: flex;
-          justify-content: space-between;
-          scrollbar-width: none; /* Firefox */
-          -ms-overflow-style: none; /* Internet Explorer 10+ */
-        }
-        ._container::-webkit-scrollbar {
-          /* WebKit */
-          width: 0;
-          height: 0;
-        }
-
-        #_background_blur {
-          z-index: 50;
-        }
-
-        @media only screen and (max-width: 780px) {
+    <Layout>
+      <div className="_container relative">
+        <IconEther />
+        <ContentBox handleIntersection={handleIntersection}>
+          <Landing
+            title={LANDING.NAME}
+            role={LANDING.ROLE}
+            description={LANDING.CONTENT}
+            keywords={LANDING.KEYWORDS}
+            isInView={currentSection === "_landing"}
+          />
+          <About isInView={currentSection === "_about"} />
+          <Product isInView={currentSection === "_product"} />
+          {/* <BlogPost isInView={currentSection === "_blogpost"} /> */}
+          <Experience isInView={currentSection === "_experience"} />
+        </ContentBox>
+        <NavBox showLanding={currentSection === "_landing"} />
+        <style jsx>{`
           ._container {
-            flex-direction: column;
-            align-items: center;
+            height: 100%;
+            width: 100%;
+            color: var(--green);
+            background-color: var(--black);
+
+            padding: 0;
+            margin: 0;
+
+            font-family: Roboto;
+            font-weight: bold;
+
+            overflow: hidden;
+            display: flex;
+            justify-content: space-between;
+            scrollbar-width: none; /* Firefox */
+            -ms-overflow-style: none; /* Internet Explorer 10+ */
           }
-        }
-      `}</style>
-      <style jsx global>{`
-        ._svg {
-          width: 100%;
-          height: 100%;
-        }
-        p {
-          white-space: pre-line;
-        }
+          ._container::-webkit-scrollbar {
+            /* WebKit */
+            width: 0;
+            height: 0;
+          }
 
-        ._contentBox {
-          display: flex;
-        }
+          #_background_blur {
+            z-index: 50;
+          }
 
-        #_about,
-        #_products,
-        #_blogpost,
-        #_experience {
-          flex-direction: row;
-          gap: 20px;
-        }
-        @media only screen and (max-width: 1100px) {
+          @media only screen and (max-width: 780px) {
+            ._container {
+              flex-direction: column;
+              align-items: center;
+            }
+          }
+        `}</style>
+        <style jsx global>{`
+          ._svg {
+            width: 100%;
+            height: 100%;
+          }
+          p {
+            white-space: pre-line;
+          }
+
+          ._contentBox {
+            display: flex;
+          }
+
           #_about,
-          #_product,
+          #_products,
           #_blogpost,
           #_experience {
-            flex-direction: column;
-            align-items: center;
-            gap: 80px;
+            flex-direction: row;
+            gap: 20px;
           }
-          #_about > div {
-            gap: 80px;
+          @media only screen and (max-width: 1100px) {
+            #_about,
+            #_product,
+            #_blogpost,
+            #_experience {
+              flex-direction: column;
+              align-items: center;
+              gap: 80px;
+            }
+            #_about > div {
+              gap: 80px;
+            }
           }
-        }
-      `}</style>
-      <div
-        className="hide"
-        id="animated_inline_svg"
-        dangerouslySetInnerHTML={{ __html: inlineSVG }}
-      />
-    </div>
+        `}</style>
+        <div
+          className="hide"
+          id="animated_inline_svg"
+          dangerouslySetInnerHTML={{ __html: inlineSVG }}
+        />
+      </div>
+    </Layout>
   );
 }
