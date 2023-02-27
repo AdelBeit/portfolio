f3e5c0e0e97403592ec966eded24b7dc59b2b055
adel <adelbeit@gmail.com>
2023-02-26 23:56:12 -0800

    heading section now has github and portfolio site links

--- src/resume/components/development/TitleSection.tsx
+++ src/resume/components/development/TitleSection.tsx
@@ -3,22 +3,24 @@ import { useDev } from "../../hooks/use_site_context";
 import styles from "./TitleSection.module.css";
 
 function TitleSection() {
-  const { name, city, github, email, phone } = useDev();
+  const { name, city, portfolio, email, github } = useDev();
 
   return (
     <div className={styles.container}>
       <div className={styles.left}>
         <h4 className={styles.city}>{city}</h4>
         <h4 className={styles.site}>
-          <a href={"https://" + github}>{github}</a>
+          <a href={"https://" + portfolio}>{portfolio}</a>
         </h4>
       </div>
       <div className={styles.center}>
         <h1>{name}</h1>
       </div>
       <div className={styles.right}>
-        <h4 className={styles.phone}>{phone}</h4>
         <h4 className={styles.email}>{email}</h4>
+        <h4 className={styles.github}>
+          <a href={"https://" + github}>{github}</a>
+        </h4>
       </div>
     </div>
   );
