4aefadef62284b778d14757763f852671b1accdb
Adele <adelbeit@gmail.com>
2023-03-29 21:49:17 -0700

    update dev data

--- src/resume/development.data.ts
+++ src/resume/development.data.ts
@@ -8,7 +8,7 @@ export const SiteData = {
   portfolio: "adelbeit.com",
   role: "Frontend Engineer",
   objective:
-    "After earning my BS in CS in 2019, I worked as a Scrum Master and Quality Engineer. Exposing me to various aspects of the SDLC process, and allowing me to develop a comprehensive understanding of it as a developer. Meanwhile, I also honed my development and design skills through working on side projects which can be found on my Github.",
+    "I have a Bachelor's degree in CS, and 2 years of corporate work experience, collaborating and leading people on multimillion dollar projects. On my downtime I work on my side projects which can be found on my Github. Although my experience is mainly in Javscript and React, I'm always looking to expand my skillset so I have no problem picking up other technologies as long as the job gets done!",
   products: [
     {
       title: "Jamroom",
