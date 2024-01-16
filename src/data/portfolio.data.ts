38a18b53c00bdffcd1c8d523e2ff57fd8e664a35
Adel Beitvashahi <AdelBeit@users.noreply.github.com>
2024-01-15 19:05:33 -0800

    Update portfolio.data.ts

--- src/data/portfolio.data.ts
+++ src/data/portfolio.data.ts
@@ -1,5 +1,5 @@
 const LANDING = {
-  NAME: "Adel Beitvashahi",
+  NAME: "Adele Beitvashahi",
   ROLE: "Software Engineer",
   CONTENT: "I have a passion for developing software that's",
   KEYWORDS: [
